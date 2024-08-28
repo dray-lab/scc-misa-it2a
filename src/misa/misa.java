31f1af2154ed7f6d1e16a8be1bf3eca553934a94
YvanClarc <yonzonivanclark@gmail.com>
2024-08-28 10:40:40 +0800

    Second Repo

---
+++ src/misa/misa.java
@@ -0,0 +1,16 @@
+package misa;
+
+public class misa {
+    
+public static  void main (String[] args){
+    
+    Product pr = new Product ();
+    pr.addProduct(1011, "Shampoo", 10, 100);
+    pr.viewProduct ();
+    
+    Product pr1 = new Product ();
+    pr1.addProduct (1012, "Soap", 10, 20);
+    pr1.viewProduct();
+    
+}
+}
